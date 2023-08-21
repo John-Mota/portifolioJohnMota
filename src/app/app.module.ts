68e1764e200677f51e2811b898fd7a08538252b2
John Mota <johnmarcos.mota@gmail.com>
2023-08-21 02:46:03 -0300

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePrincipalComponent } from './home-principal/home-principal.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { HomePrincipalComponent } from './home-principal/home-principal.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+
+import { HomePrincipalComponent } from "./home-principal/home-principal.component";
+
+
+
+export const ROUTES: Routes = [
+  {path: '', component: HomePrincipalComponent},
+]
