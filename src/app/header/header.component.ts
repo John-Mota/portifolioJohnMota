ed7abb9fde527f232d24a259a5eb2efa6c2fe009
John Mota <johnmarcos.mota@gmail.com>
2023-09-08 10:59:24 -0300

    header-component finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FunctionsService } from '../functions.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(private themeService: FunctionsService) {}
+
+  isDarkTheme: boolean = true;
+
+  ngOnInit(): void {
+    this.themeService.isDarkTheme$.subscribe(isDark => {
+      this.isDarkTheme = isDark;
+      this.applyTheme();
+    });
+  }
+
+  private applyTheme() {
+
+    if (this.isDarkTheme) {
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+    }
+  }
 
 }
